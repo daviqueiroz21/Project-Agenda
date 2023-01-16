4a61a3d085f73ff2f99dda4a67a82e1eddb1530e
Mateus Belmonte <mateus.santos@suporterenova.onmicrosoft.com>
2023-01-16 12:56:04 -0300

    Criando usuario part 2 com hash na senha e verificação de email

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-
+const bcryptjs = require('bcryptjs');
 const LoginSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true }
@@ -17,15 +17,28 @@ class Login {
 
  async register() {
     this.valida();
-
     if(this.errors.length > 0) return;
+    
+    await this.userExists();
+    
+    const salt = bcryptjs.genSaltSync();
+    this.body.password = bcryptjs.hashSync(this.body.password, salt);
 
-    try{
-      this.user = await LoginModel.create(this.body);
+  try{
+
+
+    this.user = await LoginModel.create(this.body);
     }catch(err){
       console.log(e)
     }
   };
+
+  async userExists() {
+   const user =  await LoginModel.findOne({ email: this.body.email})
+
+   if(user) this.errors.push('Usuário já existe')
+
+  }
   
   valida = ()=>{
     this.cleanUp();
